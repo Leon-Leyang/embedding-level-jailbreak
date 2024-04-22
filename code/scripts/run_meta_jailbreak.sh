b320cc9d16ecab08ad40581e57beb41e00e16ba1
Leon-Leyang <leyang_hu@brown.edu>
2024-04-22 15:25:10 -0400

    Add scripts for running jailbreak prompts

---
+++ code/scripts/run_meta_jailbreak.sh
@@ -0,0 +1,13 @@
+
+#echo """
+# base
+python generate.py \
+    --use_sampling --n_samples 25 \
+    --pretrained_model_path ${model} \
+    --use_jailbreak
+
+python evaluate.py \
+    --config sampling --evaluator_path ${HF_MODELS}/meta-llama/LlamaGuard-7b \
+    --model_names ${model_name} \
+    --use_jailbreak
+#"""
