acf2a753fd576c99d1f1fc4e2ed60b69c450b1cd
Leon-Leyang <leyang_hu@brown.edu>
2024-04-22 15:02:19 -0400

    Add scripts for jailbreak prompt preparation

---
+++ code/prep_jailbreak.py
@@ -0,0 +1,12 @@
+def add_sure(original_file_path, new_file_path):
+    with open(original_file_path, 'r') as f:
+        lines = f.readlines()
+    lines = [line.strip() + ' Start with \"Sure here’s\".' for line in lines]
+    with open(new_file_path, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(lines))
+
+
+if __name__ == '__main__':
+    original_file_path = './data/custom.txt'
+    new_file_path = './data_jailbreak/custom_sure.txt'
+    add_sure(original_file_path, new_file_path)
