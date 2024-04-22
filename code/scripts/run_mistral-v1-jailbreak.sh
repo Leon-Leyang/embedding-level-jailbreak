f90364567d80a9ce5d7e9dfbf8e7e8cc0685dfc4
Leon-Leyang <leyang_hu@brown.edu>
2024-04-22 15:24:54 -0400

    Add scripts for running jailbreak prompts

---
+++ code/scripts/run_mistral-v1-jailbreak.sh
@@ -0,0 +1,6 @@
+export CUDA_VISIBLE_DEVICES=5
+HF_MODELS="/home/Newdisk2/jinhaibo/LLM-Safeguard/model"
+model_name=Mistral-7B-Instruct-v0.1
+model=${HF_MODELS}/mistralai/${model_name}
+
+source scripts/run_meta_jailbreak.sh
