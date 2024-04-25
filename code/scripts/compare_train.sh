4d43b38cdc1092dc838e9e174859c898e9cad8c6
Leon-Leyang <leyang_hu@brown.edu>
2024-04-24 21:50:40 -0400

    Add scripts for visualizing ood performance

--- code/scripts/compare_train.sh
+++ code/scripts/compare_train.sh
@@ -3,19 +3,25 @@ HF_MODELS="/home/Newdisk2/jinhaibo/LLM-Safeguard/model"
 full_model_name=${HF_MODELS}/"meta-llama/Llama-2-7b-chat-hf"
 
 
-python compare_train_harmfulness_jailbreak.py \
-    --pretrained_model_paths ${full_model_name} \
-    --config sampling \
-    --output_path comparisons/train_jailbreak
-
-
-python compare_train_refusal_jailbreak.py \
-    --pretrained_model_paths ${full_model_name} \
-    --config sampling \
-    --output_path comparisons/train_jailbreak
-
-
-#python compare_ood_harmfulness_jailbreak.py \
+#python compare_train_harmfulness_jailbreak.py \
 #    --pretrained_model_paths ${full_model_name} \
 #    --config sampling \
-#    --output_path comparisons/ood_jailbreak
+#    --output_path comparisons/train_jailbreak
+#
+#
+#python compare_train_refusal_jailbreak.py \
+#    --pretrained_model_paths ${full_model_name} \
+#    --config sampling \
+#    --output_path comparisons/train_jailbreak
+
+
+python compare_ood_harmfulness_jailbreak.py \
+    --pretrained_model_paths ${full_model_name} \
+    --config sampling \
+    --output_path comparisons/train_jailbreak
+
+
+python compare_ood_refusal_jailbreak.py \
+    --pretrained_model_paths ${full_model_name} \
+    --config sampling \
+    --output_path comparisons/train_jailbreak
